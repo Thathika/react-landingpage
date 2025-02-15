ef8706f75ba9b480f308132bacc9fe8342f0f4c7
Thathika <thathikam,cs24@bitsathy.ac.in>
2025-02-15 11:34:45 +0530

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="App-header">
+      <h1>About Us</h1>
+      <p>We are a company dedicated to providing the best services.</p>
+    </div>
+  );
+};
+
+export default About;
